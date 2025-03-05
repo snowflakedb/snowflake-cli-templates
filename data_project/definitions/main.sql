cf865d78b231f08585d065a4c2532bc602893636
Tomasz Wazny <tomasz.wazny@snowflake.com>
2025-03-05 23:25:22 +0100

    Split single definition file into two

--- data_project/definitions/main.sql
+++
@@ -1,7 +0,0 @@
-define database {{ example_db_name }};
-
-define schema {{ example_db_name }}.sch1 COMMENT = 'some comment';
-
-define table {{ example_db_name }}.sch1.tab1 ( {{ example_column_name }} integer, col_2 varchar);
-
-define stage  {{ example_db_name }}.sch1.stg1 COMMENT = 'another comment';
